5e023ebf246285a2a303a567cad2a819dccd9e20
chengzixiao <chengzixiao2007@qq.com>
2026-05-05 17:22:05 +0800

    5

--- UserCode/Src/Command.c
+++ UserCode/Src/Command.c
@@ -16,9 +16,10 @@ uint8_t writeIndex = 0;
 * @brief 增加读索引
 * @param length 要增加的长度
 */
-void Command_AddReadIndex(uint8_t length) {
-    readIndex += length;
-    readIndex %= BUFFER_SIZE;
+void Command_AddReadIndex(uint8_t length)
+{
+	readIndex += length;
+	readIndex %= BUFFER_SIZE;
 }
 
 /**
@@ -26,9 +27,10 @@ void Command_AddReadIndex(uint8_t length) {
 * @param i 要读取的数据索引
 */
 
-uint8_t Command_Read(uint8_t i) {
-    uint8_t index = i % BUFFER_SIZE;
-    return buffer[index];
+uint8_t Command_Read(uint8_t i)
+{
+	uint8_t index = i % BUFFER_SIZE;
+	return buffer[index];
 }
 
 /**
@@ -39,8 +41,9 @@ uint8_t Command_Read(uint8_t i) {
 * @retval BUFFER_SIZE 缓冲区已满
 */
 
-uint8_t Command_GetLength() {
-    return (writeIndex + BUFFER_SIZE - readIndex) % BUFFER_SIZE;
+uint8_t Command_GetLength()
+{
+	return (writeIndex + BUFFER_SIZE - readIndex) % BUFFER_SIZE;
 }
 
 /**
@@ -50,8 +53,9 @@ uint8_t Command_GetLength() {
 * @retval 1~BUFFER_SIZE-1 剩余空间
 * @retval BUFFER_SIZE 缓冲区为空
 */
-uint8_t Command_GetRemain() {
-    return BUFFER_SIZE - Command_GetLength();
+uint8_t Command_GetRemain()
+{
+	return BUFFER_SIZE - Command_GetLength();
 }
 
 /**
@@ -60,22 +64,27 @@ uint8_t Command_GetRemain() {
 * @param length 要写入的数据长度
 * @return 写入的数据长度
 */
-uint8_t Command_Write(uint8_t *data, uint8_t length) {
-    // 如果缓冲区不足 则不写入数据 返回0
-    if (Command_GetRemain() < length) {
-        return 0;
-    }
-    // 使用memcpy函数将数据写入缓冲区
-    if (writeIndex + length < BUFFER_SIZE) {
-        memcpy(buffer + writeIndex, data, length);
-        writeIndex += length;
-    } else {
-        uint8_t firstLength = BUFFER_SIZE - writeIndex;
-        memcpy(buffer + writeIndex, data, firstLength);
-        memcpy(buffer, data + firstLength, length - firstLength);
-        writeIndex = length - firstLength;
-    }
-    return length;
+uint8_t Command_Write(uint8_t *data, uint8_t length)
+{
+	// 如果缓冲区不足 则不写入数据 返回0
+	if (Command_GetRemain() < length)
+	{
+		return 0;
+	}
+	// 使用memcpy函数将数据写入缓冲区
+	if (writeIndex + length < BUFFER_SIZE)
+	{
+		memcpy(buffer + writeIndex, data, length);
+		writeIndex += length;
+	}
+	else
+	{
+		uint8_t firstLength = BUFFER_SIZE - writeIndex;
+		memcpy(buffer + writeIndex, data, firstLength);
+		memcpy(buffer, data + firstLength, length - firstLength);
+		writeIndex = length - firstLength;
+	}
+	return length;
 }
 
 /**
@@ -84,43 +93,53 @@ uint8_t Command_Write(uint8_t *data, uint8_t length) {
 * @return 获取的指令长度
 * @retval 0 没有获取到指令
 */
-uint8_t Command_GetCommand(uint8_t *command) {
-    // 寻找完整指令
-    while (1) {
-        // 如果缓冲区长度小于COMMAND_MIN_LENGTH 则不可能有完整的指令
-        if (Command_GetLength() < COMMAND_MIN_LENGTH) {
-					return 0;
-        }
-        // 如果不是包头 则跳过 重新开始寻找
-        if (Command_Read(readIndex) != 0x5A || Command_Read(readIndex + 1) != 0xA5) {
-					Command_AddReadIndex(1);
-					continue;
-        }
-        // 如果缓冲区长度小于指令长度 则不可能有完整的指令
-        uint8_t length = 0;
-				if(Command_Read(readIndex + 2) == 0x01){
-					length = 5;
-				}
-				if(Command_Read(readIndex + 2) == 0x02){
-					length = 8;
-				}
-        if (Command_GetLength() < length) {
-        return 0;
-        }
-        // 如果校验和不正确 则跳过 重新开始寻找
-					uint8_t sum = 0;
-					for (uint8_t i = 0; i < length - 1; i++) {
-					sum += Command_Read(readIndex + i);
-        }
-					if (sum != Command_Read(readIndex + length - 1)) {
-					Command_AddReadIndex(1);
-					continue;
-        }
-        // 如果找到完整指令 则将指令写入command 返回指令长度
-        for (uint8_t i = 0; i < length; i++) {
-					command[i] = Command_Read(readIndex + i);
-        }
-        Command_AddReadIndex(length);
-        return length;
-    }
+uint8_t Command_GetCommand(uint8_t *command)
+{
+	// 寻找完整指令
+	while (1)
+	{
+		// 如果缓冲区长度小于COMMAND_MIN_LENGTH 则不可能有完整的指令
+		if (Command_GetLength() < COMMAND_MIN_LENGTH)
+		{
+			return 0;
+		}
+		// 如果不是包头 则跳过 重新开始寻找
+		if (Command_Read(readIndex) != 0x5A || Command_Read(readIndex + 1) != 0xA5)
+		{
+			Command_AddReadIndex(1);
+			continue;
+		}
+		// 如果缓冲区长度小于指令长度 则不可能有完整的指令
+		uint8_t length = 0;
+		if(Command_Read(readIndex + 2) == 0x01)
+		{
+			length = 5;
+		}
+		if(Command_Read(readIndex + 2) == 0x02)
+		{
+			length = 8;
+		}
+		if (Command_GetLength() < length)
+		{
+			return 0;
+		}
+		// 如果校验和不正确 则跳过 重新开始寻找
+		uint8_t sum = 0;
+		for (uint8_t i = 0; i < length - 1; i++)
+		{
+			sum += Command_Read(readIndex + i);
+		}
+		if (sum != Command_Read(readIndex + length - 1))
+		{
+			Command_AddReadIndex(1);
+			continue;
+		}
+		// 如果找到完整指令 则将指令写入command 返回指令长度
+		for (uint8_t i = 0; i < length; i++)
+		{
+			command[i] = Command_Read(readIndex + i);
+		}
+		Command_AddReadIndex(length);
+		return length;
+	}
 }
